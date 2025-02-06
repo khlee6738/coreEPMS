c16644e44d7cca93faf1ae1dd7c32c652fda432e
LeeKyuHo <khlee6738@naver.com>
2025-02-06 13:32:31 +0900

    first commit

---
+++ src/main/java/com/newlecture/web/CoreEpmsApplication.java
@@ -0,0 +1,13 @@
+package com.newlecture.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreEpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreEpmsApplication.class, args);
+	}
+
+}
